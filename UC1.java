23d18e133f011d06d76e85727fb3024dc670c99f
Ekansh Patil <ekansh.patil@outlook.com>
2026-03-17 22:44:46 +0530

    Create OOPSBannerApp to print 'OOPS' to console
    This class demonstrates basic Java program structure.

---
+++ UC1.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp is a simple Java application that prints "OOPS" to the console.
+ *
+ * <p>This class demonstrates basic Java program structure including
+ * a main method as the entry point of the application.</p>
+ *
+ * @author JavaDeveloper
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main method serves as the entry point of the application.
+     *
+     * @param args command-line arguments (not used in this application)
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
